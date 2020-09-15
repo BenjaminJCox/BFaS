89cb27ba29a3907611c222eed8cadda009a39b81
AndrewTijua <ben.10.10@hotmail.co.uk>
2020-09-15 03:58:58 +0100

    untested but idgaf

--- src/kf.jl
+++ src/kf.jl
@@ -71,6 +71,22 @@ psi: function mapping prior mean to model mean
 Output no longer has predictive likelihood as this is not meaningful for approximate filters
 =#
 
+#=
+Input:
+x - Nx1 state mean of previous step
+P - NxN state covariance estimate of pervious step
+y - Dx1 Measurement vector
+H - Observation Matrix
+R - Measurement noise covariance
+
+Output:
+x - Updated State Mean
+P - Updated State Covariance
+K - Kalman Gain
+IM - Predictive mean of y
+IS - Covariance of predictive mean of y
+=#
+
 function threeDVAR_predict(x::Vector{Float64}, P::Matrix, psi = x -> x, Q = zeros(size(x, 1), size(x, 1)), B = [], u = [])
     if (isempty(B) && !isempty(u))
         B = zeros(size(x, 1), size(u, 1))
@@ -99,6 +115,24 @@ end
 #=
 Because Julia is awesome we can take gradients of very general code, meaning that the extended Kalman filter remains useful for a rather
 long time in this context
+
+todo: store jacobian information so that calculation is MUCH MUCH faster, should be ok tbh but not a priority
+=#
+
+#=
+Input:
+x - Nx1 state mean of previous step
+P - NxN state covariance estimate of pervious step
+y - Dx1 Measurement vector
+H - Observation Matrix
+R - Measurement noise covariance
+
+Output:
+x - Updated State Mean
+P - Updated State Covariance
+K - Kalman Gain
+IM - Predictive mean of y
+IS - Covariance of predictive mean of y
 =#
 
 function exkf_predict(x::Vector{Float64}, P::Matrix, psi = x -> x, Q = zeros(size(x, 1), size(x, 1)))
@@ -117,7 +151,7 @@ function exkf_update(x::Vector{Float64}, P::Matrix{Float64}, y::Vector{Float64},
     return (xp, Pp, K, IM, IS)
 end
 
-function exkf_update(x::Vector{Float64}, P::Matrix{Float64}, y::Vector{Float64}, H, R::Matrix{Float64})
+function exkf_update(x::Vector{Float64}, P::Matrix{Float64}, y::Vector{Float64}, H::Function, R::Matrix{Float64})
     H_jac = ForwardDiff.jacobian(H, x)
     v = y - H(x)
     IM = H(x)
@@ -127,3 +161,123 @@ function exkf_update(x::Vector{Float64}, P::Matrix{Float64}, y::Vector{Float64},
     Pp = P - K * IS * K'
     return (xp, Pp, K, IM, IS)
 end
+
+#=
+This filter is less stanky than others. Because it is unscented. Am I humourous Father?!
+
+DON'T USE THE FUNCTIONS BELOW EXCEPT THE PREDICT AND UPDATE ONES, THEY ARE JUST FOR CONVIENIENCE
+=#
+
+function ukf_generate_sigma_points(x::Vector{Float64}, P::Matrix{Float64}; alpha::Float64 = 1.0, kappa::Float64 = 3.0 - size(x, 1))
+    n = size(x, 1)
+    sigma_point_matrix = zeros(2 * size(x, 1) + 1, 2 * size(x, 1) + 1)
+    sigma_point_matrix[:, 1] = x
+    srp = sqrt(P) # this is fine as P is guaranteed to be symmetric by virtue of being a covariance matrix
+    lambda = alpha^2 * (n + kappa) - n
+    for i = 1:n
+        sigma_point_matrix[:, i+1] = x + sqrt(n + lambda) * srp[:, i]
+        sigma_point_matrix[:, i+1+n] = x - sqrt(n + lambda) * srp[:, i]
+    end
+    return sigma_point_matrix
+end
+
+#this should get inlined but idec
+function ukf_propagate_sigma_points(sigma_point_matrix::Matrix{Float64}, f::Function = x -> x)
+    mapped_sigma_points = mapslices(f, sigma_point_matrix, dims = 1)
+    return mapped_sigma_points
+end
+
+function ukf_weights(n::Int64; alpha::Float64 = 1.0, kappa::Float64 = 3.0 - size(x, 1), beta::Float64 = 0.)
+    weight_m_vector = ones(2n+1)
+    weight_c_vector = ones(2n+1)
+
+    wv = 1/(2*n + 2*lambda)
+
+    weight_m_vector .*= wv
+    weight_c_vector .*= wv
+
+
+    weight_m_vector[1] = lambda / (n+lambda)
+    weight_c_vector[1] = lambda / (n+lambda) + (1 - alpha^2 + beta)
+
+    return @dict weight_m_vector weight_c_vector
+end
+
+function ukf_pred_mean_cv(sigma_points::Matrix{Float64}, weight_dict, Q::Matrix{Float64})
+    n = size(sigma_points, 1)
+    mkd = zeros(n)
+
+    weight_m_vector = weight_dict[:weight_m_vector]
+    weight_c_vector = weight_dict[:weight_c_vector]
+
+    for i = 1:(2n+1)
+        mkd += weight_m_vector[i] * sigma_points[:, i]
+    end
+
+    Pkd = Q
+    for i = 1:(2n+1)
+        mcs = sigma_points[:, i] - mkd
+        Pkd += weight_c_vector[i] * mcs * mcs'
+    end
+
+    return (mkd, Pkd)
+end
+
+function ukf_upda_mean_cvneas_ccv_statemeas(mkd::Vector{Float64}, sigma_points::Matrix{Float64}, measuremodel_sigma_points::Matrix{Float64}, weight_dict, R::Matrix{Float64})
+    n = size(sigma_points, 1)
+    muk = zeros(n)
+
+    weight_m_vector = weight_dict[:weight_m_vector]
+    weight_c_vector = weight_dict[:weight_c_vector]
+
+    for i = 1:(2n+1)
+        muk += weight_m_vector[i] * measuremodel_sigma_points[:, i]
+    end
+
+    Sk = R
+    for i = 1:(2n+1)
+        mcs = measuremodel_sigma_points[:, i] - muk
+        Sk += weight_c_vector[i] * mcs * mcs'
+    end
+
+    Ck = zeros(size(Sk))
+    for i = 1:(2n+1)
+        Ck += weight_c_vector[i] * (sigma_points[:, i] - x) * (measuremodel_sigma_points[:, i] - muk)''
+    end
+    return (muk, Sk, Ck)
+end
+
+function ukf_predict(x::Vector{Float64}, P::Matrix, psi = x -> x, Q = zeros(size(x, 1), size(x, 1)); alpha::Float64 = 1.0, kappa::Float64 = 3.0 - size(x, 1), beta::Float64 = 0.)
+    sigma_points = ukf_generate_sigma_points(x, P, alpha, kappa)
+    mapped_sigma_points = ukf_propagate_sigma_points(sigma_points, psi)
+    weights = ukf_weights(size(x,1), alpha, kappa, beta)
+    (mp, Pp) = ukf_pred_mean_cv(mapped_sigma_points, weights, Q)
+    return (mp, Pp)
+end
+
+function ukf_update(x::Vector{Float64}, P::Matrix{Float64}, y::Vector{Float64}, H, R::Matrix{Float64}; alpha::Float64 = 1.0, kappa::Float64 = 3.0 - size(x, 1), beta::Float64 = 0.)
+    sigma_points = ukf_generate_sigma_points(x, P, alpha, kappa)
+    mapped_sigma_points = ukf_propagate_sigma_points(sigma_points, H)
+    weights = ukf_weights(size(x,1), alpha, kappa, beta)
+    (muk, Sk, Ck) = ukf_upda_mean_cvneas_ccv_statemeas(x, sigma_points, mapped_sigma_points, weights, R)
+    Kk = Ck / Sk
+    mk = x + Kk * (y - muk)
+    Pk = P - Kk * Sk * Kk'
+    return (mk, Pk, Kk, muk, Sk)
+end
+
+#=
+Input:
+x - Nx1 state mean of previous step
+P - NxN state covariance estimate of pervious step
+y - Dx1 Measurement vector
+H - Observation Matrix
+R - Measurement noise covariance
+
+Output:
+x - Updated State Mean
+P - Updated State Covariance
+K - Kalman Gain
+IM - Predictive mean of y
+IS - Covariance of predictive mean of y
+=#
